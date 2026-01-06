dae06262095b093c3bb4dfd75906a9d6342452a1
mahadi40011 <mahadi40011@gmail.com>
2026-01-06 07:30:02 +0600

    Create Home Component and add to the Router. Setup RouterProvider and Mainlayout

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,9 +1,12 @@
-import React from 'react';
+import React from "react";
+import { Outlet } from "react-router";
 
 const MainLayout = () => {
   return (
     <div>
-      
+      <main>
+        <Outlet />
+      </main>
     </div>
   );
 };
---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div className='font-bold text-7xl flex justify-center items-center h-screen'>
+      home
+    </div>
+  );
+};
+
+export default Home;
